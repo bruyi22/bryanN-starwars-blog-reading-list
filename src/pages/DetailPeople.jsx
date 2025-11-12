d69edae3dfcb0dc3fe2e2ffe0e76cd9c42579746
Bryan Navarro <diazbnavarro@gmail.com>
2025-11-12 01:49:19 +0000

    fixed detailplanet error

--- src/pages/DetailPeople.jsx
+++ src/pages/DetailPeople.jsx
@@ -25,7 +25,6 @@ export const DetailPeople = () => {
     return (
 
         <div>
-            <h1 className="text-danger ms-5">DetailPlanet</h1>
             <div className="mx-5 mb-3 ">
                 <div className=" mx-5 row g-0 p-4 border-bottom border-danger">
                     <div className=" mx-5 col-md-3">
@@ -34,7 +33,9 @@ export const DetailPeople = () => {
                     <div className="col-md-8">
                         <div className="card-body">
                             <h1 className="card-title d-flex justify-content-center mb-5">Name: {character?.name}</h1>
-                            <p className="card-text mx-auto text-wrap text-center" style={{width: '38rem'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit quidem ullam ratione aliquam possimus laborum! Laboriosam expedita, velit perspiciatis similique inventore quasi fugiat eaque accusantium enim. Recusandae, magnam! Voluptates, fuga.</p>
+                            <p className="card-text mx-auto text-wrap text-center" style={{width: '38rem'}}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aut eligendi id eius magni. Repellendus quod tempora iste architecto hic. Numquam non sit magni praesentium qui similique ea officiis quos nulla ipsa! Quis repellendus dolores incidunt reprehenderit odit exercitationem debitis illum a rerum impedit, cumque architecto aliquid sit ipsum sed ut pariatur excepturi laudantium ipsam officia voluptatibus eos? Maxime maiores facilis deserunt sapiente sit quam vero, consequuntur quasi. Saepe accusamus provident at officiis non molestias esse cumque dignissimos vel illum delectus, commodi praesentium et, sit dolorum soluta assumenda a tenetur ad. Odit doloribus facere sapiente vitae quo sed accusantium nemo!
+                            </p>
                         </div>
                     </div>
                 </div>
--- src/pages/DetailPlanet.jsx
+++ src/pages/DetailPlanet.jsx
@@ -7,7 +7,7 @@ const DetailPlanet = () => {
 
     const [planet, setPlanet] = useState(null)
     function detailPlanet() {
-        fetch("https://www.swapi.tech/api/planet/" + uid)
+        fetch("https://www.swapi.tech/api/planets/" + uid)
             .then(res => res.json())
             .then(data => setPlanet(data.result.properties))
             .catch(err => console.error(err))
@@ -19,18 +19,49 @@ const DetailPlanet = () => {
 
     return (
         <div>
-            <h1>DetailPlanet</h1>
-            <div className="container">
-                <p>Name: {planet?.name}</p>
-                <div class="card" style={{width: "18rem"}}>
-                    <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people/${uid}.jpg`} style={{width: "18rem", height: '100rem'}} class="card-img-top" alt="..."/>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <div className="mx-5 mb-3 ">
+                <div className=" mx-5 row g-0 p-4 border-bottom border-danger">
+                    <div className=" mx-5 col-md-3">
+                        <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/planet/${uid}.jpg`} className="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h1 className="card-title d-flex justify-content-center mb-5">Name: {planet?.name}</h1>
+                            <p className="card-text mx-auto text-wrap text-center" style={{width: '38rem'}}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam aut eligendi id eius magni. Repellendus quod tempora iste architecto hic. Numquam non sit magni praesentium qui similique ea officiis quos nulla ipsa! Quis repellendus dolores incidunt reprehenderit odit exercitationem debitis illum a rerum impedit, cumque architecto aliquid sit ipsum sed ut pariatur excepturi laudantium ipsam officia voluptatibus eos? Maxime maiores facilis deserunt sapiente sit quam vero, consequuntur quasi. Saepe accusamus provident at officiis non molestias esse cumque dignissimos vel illum delectus, commodi praesentium et, sit dolorum soluta assumenda a tenetur ad. Odit doloribus facere sapiente vitae quo sed accusantium nemo!
+                            </p>
                         </div>
+                    </div>
                 </div>
-                
+                <div className="container text-danger">
+                    <div className="d-flex flex-row mx-5 mb-3 justify-content-evenly">
+                        <div className="text-center">
+                            <p>Name</p>
+                            <h5 className="p-2">{planet?.name}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Gravity</p>
+                            <h5 className="p-2">{planet?.gravity}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Climate</p>
+                            <h5 className="p-2">{planet?.climate}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Population</p>
+                            <h5 className="p-2">{planet?.population}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Orbital Period</p>
+                            <h5 className="p-2">{planet?.orbital_period}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Terrain</p>
+                            <h5 className="p-2">{planet?.terrain}</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
