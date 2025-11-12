9d33d202f3857d846afce493dae0466412092ac4
Bryan Navarro <diazbnavarro@gmail.com>
2025-11-12 01:35:54 +0000

    working on visual for details

--- src/pages/DetailPeople.jsx
+++ src/pages/DetailPeople.jsx
@@ -23,11 +23,51 @@ export const DetailPeople = () => {
 
 
     return (
-        
-            <div className="container">
-                <p>Name: {character?.name}</p>
-                 <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people/${uid}.jpg`} className="card-img-top" alt="..." />
+
+        <div>
+            <h1 className="text-danger ms-5">DetailPlanet</h1>
+            <div className="mx-5 mb-3 ">
+                <div className=" mx-5 row g-0 p-4 border-bottom border-danger">
+                    <div className=" mx-5 col-md-3">
+                        <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people/${uid}.jpg`} className="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h1 className="card-title d-flex justify-content-center mb-5">Name: {character?.name}</h1>
+                            <p className="card-text mx-auto text-wrap text-center" style={{width: '38rem'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit quidem ullam ratione aliquam possimus laborum! Laboriosam expedita, velit perspiciatis similique inventore quasi fugiat eaque accusantium enim. Recusandae, magnam! Voluptates, fuga.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="container text-danger">
+                    <div className="d-flex flex-row mx-5 mb-3 justify-content-evenly">
+                        <div className="text-center">
+                            <p>Name</p>
+                            <h5 className="p-2">{character?.name}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Height</p>
+                            <h5 className="p-2">{character?.height}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Hair Color</p>
+                            <h5 className="p-2">{character?.hair_color}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Eyes Color</p>
+                            <h5 className="p-2">{character?.eye_color}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Birth Year</p>
+                            <h5 className="p-2">{character?.birth_year}</h5>
+                        </div>
+                        <div className="text-center">
+                            <p>Gender</p>
+                            <h5 className="p-2">{character?.gender}</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
-        
+        </div>
+
     );
 };
--- src/pages/DetailPlanet.jsx
+++ src/pages/DetailPlanet.jsx
@@ -22,7 +22,13 @@ const DetailPlanet = () => {
             <h1>DetailPlanet</h1>
             <div className="container">
                 <p>Name: {planet?.name}</p>
-                <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people/${uid}.jpg`} className="card-img-top" alt="..." />
+                <div class="card" style={{width: "18rem"}}>
+                    <img src={`https://raw.githubusercontent.com/breatheco-de/swapi-images/master/public/images/people/${uid}.jpg`} style={{width: "18rem", height: '100rem'}} class="card-img-top" alt="..."/>
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                        </div>
+                </div>
+                
             </div>
 
         </div>
